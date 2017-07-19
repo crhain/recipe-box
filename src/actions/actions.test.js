2eebc0c76d111e044872de28cdbeba4adbb65bcb
Carl Hain <crhain@gmail.com>
2017-07-19 16:07:39 -0500

    Add actions unit tests

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,20 +2,48 @@ import actions from '.';
 import types from '../constants/';
 
 describe('Actions', () => {
+    
+     
+
     it('should create an action to filter recipes', () => {
+        const searchTerm = "Beef Stroganof";
+        const expectedAction = {
+            type: types.FILTER_RECIPES,
+            term: searchTerm
+        };
+
+        expect(actions.filterRecipes(searchTerm)).toEqual(expectedAction);
 
     });
 
     it('should create an action to add a recipe', () => {
+        const recipe = {};
+        const expectedAction = {
+            type: types.ADD_RECIPE,
+            recipe: recipe
+        };
 
+        expect(actions.addRecipe(recipe)).toEqual(expectedAction);
     });
 
     it('should create an action to delete a recipe', () => {
+        const expectedAction = {
+            type: types.DELETE_RECIPE,
+            id: 1
+        };
 
+        expect(actions.deleteRecipe(id)).toEqual(expectedAction);
     });
 
     it('should create an action to edit a recipe', () => {
+        const recipe = {};
+        const expectedAction = {
+            type: types.EDIT_RECIPE,
+            id: 1,
+            recipe: recipe
+        };
 
+        expect(actions.filterRecipes(id, recipe)).toEqual(expectedAction);
     });
 
 
