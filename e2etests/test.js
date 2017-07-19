27ea39c7f4a46d6b9fc5e664f37a2c7e6da29d22
Carl Hain <crhain@gmail.com>
2017-07-19 15:46:29 -0500

    Setup scafolding for e2e tests using wdiodriver, mocha, and selenium

---
+++ e2etests/test.js
@@ -0,0 +1,20 @@
+const expect = require('chai').expect;
+
+describe('RecipeBox App', () => {
+    it('should all me to filter a recipe from the list', () => {
+        
+    });
+
+    it('should all me to create a new recipe', () => {
+
+    });
+
+    it('should all me to delete a recipe', () => {
+        
+    });
+
+    it('should all me to edit a recipe', () => {
+        
+    });
+
+});
