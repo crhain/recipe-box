ab9efd1649e467b46ee4c619186646259f19a4e2
Carl Hain <crhain@gmail.com>
2018-05-02 14:37:48 -0500

    Add error checking into reducers; they should just return existing state if something goes wrong

--- src/reducers/recipeListReducer.js
+++ src/reducers/recipeListReducer.js
@@ -13,54 +13,48 @@ export default (state = [], action) => {
             return state;
         //action contains **recipe** to be added                                              
         case ADD_RECIPE:            
-            exists = state.find( recipe => {
-                return recipe.id === action.recipe.id;
-            });
+            // exists = state.find( recipe => {
+            //     return recipe.id === action.recipe.id;
+            // });
             
             if(exists){
                 return state;
             } 
-            
-            // return state.concat([action.recipe]);
-            return Model.addRecipe(action.recipe);
+
+            let added = Model.addRecipe(action.recipe);
+            if(added.error){
+                return state;
+            }
+                        
+            return added
 
         //action contains **recipe** to be updated                           
         case EDIT_RECIPE:
-            exists = state.find( recipe => {
-                return recipe.id === action.recipe.id;
-            });
+            // exists = state.find( recipe => {
+            //     return recipe.id === action.recipe.id;
+            // });
 
-            if(exists){
-                // return state.map( recipe => {
-                //     if(recipe.id === action.recipe.id){
-                //         return action.recipe;
-                //     }
-
-                //     return recipe;
-                // });
-                return Model.editRecipe(action.recipe);
+            
+            let edited = Model.editRecipe(action.recipe);
+            if(edited.error){
+                return state;
             }
-
-            return state;
+            
+            return edited;
 
         //action continas the **id** of a recipe to be deleted    
         case DELETE_RECIPE:
-            exists = state.find( recipe => {
-                return recipe.id === action.id;
-            });
-
-            if(exists){
-                // let newState = state.filter( recipe => recipe.id != action.id );
-                let newState = Model.deleteRecipeById(action.id);
-                // console.log("My updated state after delete is");
-                // console.log(newState);
-                action.history.push("/");
-                return newState;
-                
-            }
-
-            return state;        
-
+            // exists = state.find( recipe => {
+            //     return recipe.id === action.id;
+            // });
+                        
+            let deleted = Model.deleteRecipeById(action.id);
+                        
+            if(deleted.error){
+                return state;        
+            }            
+            action.history.push("/");
+            return deleted;
         default:
             return state;
     }
