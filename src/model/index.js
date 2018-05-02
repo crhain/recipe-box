c4b3046ad4eecc8c7572ad41e7fe691c573b6997
Carl Hain <crhain@gmail.com>
2018-05-02 14:26:45 -0500

    Add error checking to model/index.js

--- src/model/index.js
+++ src/model/index.js
@@ -66,6 +66,11 @@ Model.getAllRecipes = function(){
 }
 
 Model.addRecipe = function(recipe){
+    
+    if(model.findIndex( item => item.id === recipe.id ) > -1){    
+        return { error: "Already exists" };
+    } 
+    
     model.push(recipe);
     return model;
 }
@@ -73,32 +78,32 @@ Model.addRecipe = function(recipe){
 Model.editRecipe = function(recipe){
     //find recipe in model
     let matchPosition = model.findIndex( matchingRecipe => matchingRecipe.id === recipe.id  );
-    if(matchPosition !== -1){
-        model[matchPosition] = recipe;
+    if(matchPosition === -1){
+        return { error: "Does not exist" };    
     }
 
+    model[matchPosition] = recipe;
     return model;
 }
 
 Model.getRecipeById = function(id){
     let recipe = model.filter( recipe => recipe.id === id);
-    if(recipe.length > 0){
-        return recipe[0];
-    }
-    
-    return {};
+    if(recipe.length < 1){
+        return { error: "Recipe does not exist" };
+    }    
+    return recipe[0];    
 }
 
 Model.deleteRecipeById = function(id){
     //find recipe in model
-    let targetRecipeIndex = model.findIndex( recipe => recipe.id === id );
+    let matchPosition = model.findIndex( recipe => recipe.id === id );
+    if(matchPosition === -1){
+        return { error: "Recipe does not exist" };
+    }
+
     //splice it out
-    model.splice(targetRecipeIndex, 1);
+    model.splice(matchPosition, 1);
     return model;
 }
-
-
-
-
 
 export default Model;
