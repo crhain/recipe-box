e2d6876599ffc0e007dd8135d5ff28d8061b9a68
Carl Hain <crhain@gmail.com>
2018-05-22 13:42:45 -0500

    Add stubs for e2e tests

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,17 +1,68 @@
 const expect = require('chai').expect;
 
 describe('RecipeBox App', () => {
-    it('should all me to filter a recipe from the list', () => {
+    describe('Recipes View', () => {
+        it('should correctly display a list of recipes', () => {
         
-    });
+        });
 
-    it('should all me to create a new recipe', () => {
-
-    });
-
-    it('should all me to delete a recipe', () => {
+        it('should link to associated recipe view when clicking on recipe name', () => {
         
+        });
+
+        
+        it('should display new recipe page when clicking on new button', () => {
+            
+        });
+
     });
+
+    describe('Recipe View', () => {
+        it('should display correct recipe view for recipe associated with page', () => {
+    
+        });
+
+        it('should allow user to click on delete button and remove recipe', () => {
+    
+        });
+
+        it('should allow user to click on edit button and take user to edit page', () => {
+    
+        });
+
+        it('should allow user to click on back button and return to recipe list', () => {
+
+        });
+
+    });
+
+    describe('New Recipe', () => {
+        it('should allow user to input new recipe and add new recipe by clicking on submit button', () => {
+
+        });
+
+        it('should allow user to return to recipe list by clicking on back button', () => {
+
+        });
+    });
+
+    describe('Edit Recipe', () => {
+        it('should display correct recipe data in form', () => {
+
+        });
+
+        it('should update recipe with new data when submit is clicked', () => {
+
+        });
+
+        it('should allow user to return to recipe list by clicking on back button', () => {
+
+        });
+    
+    });
+
+    
+    
 
     it('should all me to edit a recipe', () => {
         
