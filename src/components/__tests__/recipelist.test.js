a5049ee2bc078b28b95597b1faed38d07d8b911b
Carl Hain <crhain@gmail.com>
2018-05-26 15:02:37 -0500

    Add tests for RecipeList component

--- src/components/__tests__/recipelist.test.js
+++ src/components/__tests__/recipelist.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import model  from "model/seed.js";
 import { RecipeList } from 'components/RecipeList';
+import AddButton from 'components/Buttons/AddButton';
 
 describe('RecipeList', () => {
   
@@ -13,8 +14,20 @@ describe('RecipeList', () => {
         component = shallow(<RecipeList getRecipes={ mockGetRecipesAction } recipes={ mockRecipes } />);
     });
 
-    it('renders correctly', () => {
+    it('renders without crashing', () => {
         expect(component.exists()).toEqual(true);
+    });
+
+    it('It shows a page title, and add new recipe button, and a list of recipes', () => {
+        expect(component.find('h1').length).toEqual(1); 
+        expect(component.containsMatchingElement(<AddButton />)).toEqual(true); 
+        expect(component.find('ul').length).toEqual(1); 
+    });
+
+    it('It shows a list of recipes that match the recipes state passed to the component', () => {
+        let recipeListElements = component.find('ul li');
+        expect(recipeListElements.length).toEqual(mockRecipes.length);
+        expect(recipeListElements.everyWhere((el, i) => el.text() === mockRecipes[i]['title'])).toEqual(true);
     });
 
 });
