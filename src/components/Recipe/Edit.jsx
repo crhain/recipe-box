d22dc1ecf513deb2a2d1c56d7e8edf4cf62b8ebc
Carl Hain <crhain@gmail.com>
2018-06-11 22:35:22 -0500

    Fix Edit from RecipeListing so that it works correctly by getting recipe state directly on edit mount and recipeview mount

--- src/components/Recipe/Edit.jsx
+++ src/components/Recipe/Edit.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import { editRecipe } from 'actions/';
+import { editRecipe, getRecipe } from 'actions/';
 import fields from "components/Form/fields";
 import Form from "components/Form/Form";
 import BackButton from "components/Buttons/BackButton";
 
 export class Edit extends Component{
+
+    componentWillMount(){             
+        this.props.getRecipe(this.props.match.params.id);                                                                
+    }
+ 
     convertIngredientListToForm(ingredientsList){
         return ingredientsList.join("\n");
     }
@@ -14,10 +19,14 @@ export class Edit extends Component{
         const { editRecipe, history, recipe } = this.props;
 
         let initialRecipeValues = {};
-        initialRecipeValues.id = recipe.id;
-        initialRecipeValues.title = recipe.title;
-        initialRecipeValues.servings = recipe.servings;
-        initialRecipeValues.ingredients = this.convertIngredientListToForm(recipe.ingredients);
+
+        if(recipe){
+            initialRecipeValues.id = recipe.id;
+            initialRecipeValues.title = recipe.title;
+            initialRecipeValues.servings = recipe.servings;
+            initialRecipeValues.ingredients = this.convertIngredientListToForm(recipe.ingredients);
+        }
+        
         return (
             <div>
                 <h2>Edit Recipe</h2>
@@ -38,4 +47,4 @@ function mapStateToProps({ recipe }){
 }
 
 // export default connect(null, { editRecipe })(withRouter(Edit));
-export default withRouter(connect(mapStateToProps, { editRecipe })(Edit));
+export default withRouter(connect(mapStateToProps, { editRecipe, getRecipe })(Edit));
