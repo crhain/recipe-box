710fe2840eb6600406a1ee7b281923577d64cc60
Carl Hain <crhain@gmail.com>
2018-06-13 23:47:47 -0500

    Fix problem with Edit.jsx form not rendering with recipe data when called directly

--- src/components/Recipe/Edit.jsx
+++ src/components/Recipe/Edit.jsx
@@ -1,54 +1,65 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link, withRouter } from 'react-router-dom';
-import { editRecipe, getRecipe } from 'actions/';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { getRecipe, editRecipe } from "actions/";
 import fields from "components/Form/fields";
 import Form from "components/Form/Form";
 import BackButton from "components/Buttons/BackButton";
 
-export class Edit extends Component{
 
-    componentWillMount(){    
-        console.log(this.props.match.params.id);         
+export class Edit extends Component{
+    componentDidMount(){             
         this.props.getRecipe(this.props.match.params.id);                                                                
     }
- 
-    convertIngredientListToForm(ingredientsList){
-        return ingredientsList.join("\n");
+  
+    constructor(props){
+        super(props);                
+        // this.recipe = null;      
+        this.recipeId = this.props.match.params.id;   
+        this.history = this.props.history;   
+        this.initialRecipeValues = {};
+
     }
+
+    convertIngredientListToForm(ingredientsList){        
+        return ingredientsList ? ingredientsList.join("\n") : "";
+    }
+
     render(){
-        const { editRecipe, history, recipe } = this.props;
-        console.log("Recipe is...");
-        console.log(recipe);
-
-        let initialRecipeValues;
-
-        if(recipe.id){
-            initialRecipeValues = {};
-            initialRecipeValues.id = recipe.id;
-            initialRecipeValues.title = recipe.title;
-            initialRecipeValues.servings = recipe.servings;
-            initialRecipeValues.ingredients = this.convertIngredientListToForm(recipe.ingredients);
-        }
         
+        if(this.props.recipe.id){
+            this.initialRecipeValues.id = this.props.recipe.id;
+            this.initialRecipeValues.title = this.props.recipe.title;
+            this.initialRecipeValues.servings = this.props.recipe.servings;
+            this.initialRecipeValues.ingredients = this.convertIngredientListToForm(this.props.recipe.ingredients);    
+        }
+
         return (
             <div>
-                <h2>Edit Recipe</h2>
-                <Form 
-                    fields={fields}
-                    formHandler={ editRecipe } 
-                    history={ history }
-                    intialRecipeValues={ initialRecipeValues ? initialRecipeValues : "" }
-                />
+                <h1>Edit: { this.props.recipe ? this.props.recipe.title : "" }</h1>                
+                <div>
+                    
+                </div>                
                 <BackButton />
-            </div>   
+                <div>
+                    <Form 
+                        fields={fields}
+                        formHandler={ this.props.editRecipe } 
+                        history={ this.history }
+                        intialRecipeValues={ this.initialRecipeValues ? this.initialRecipeValues : "" }
+                    />
+                </div>
+            </div>
         );
-    }    
+    }
+    
 }
+
 
 function mapStateToProps({ recipe }){
     return { recipe };
 }
 
-// export default connect(null, { editRecipe })(withRouter(Edit));
-export default withRouter(connect(mapStateToProps, { editRecipe, getRecipe })(Edit));
+export default withRouter(connect(mapStateToProps, { getRecipe, editRecipe })(Edit));
+
+
