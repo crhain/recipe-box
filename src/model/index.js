c06d9973a6cebad97c6c4b8bc9e133d93251d29c
Carl Hain <crhain@gmail.com>
2018-07-10 14:37:47 -0500

    Change 'model' variable to 'recipes' in model/index.js file to clarify what is actually being used

--- src/model/index.js
+++ src/model/index.js
@@ -1,10 +1,9 @@
-
-import model from "model/seed.js";
+import recipes from "model/seed.js";
 
 let Model = {};
 
 Model.getAllRecipes = function(){
-    //1. check to see if model has items in it
+    //1. check to see if recipes has items in it
     
     //1a. if not, get recipes key from local storage
     
@@ -12,37 +11,37 @@ Model.getAllRecipes = function(){
     
     //1a. iterate over keys and add to Model
 
-    //2. return model
-    return model;
+    //2. return recipes
+    return recipes;
 };
 
 Model.addRecipe = function(recipe){
-    //look for recipe in model and if it exists, then return error
-    if(model.findIndex( item => item.id === recipe.id ) > -1){    
+    //look for recipe in recipes and if it exists, then return error
+    if(recipes.findIndex( item => item.id === recipe.id ) > -1){    
         return { error: "Already exists" };
     }   
 
-    //push recipe to model
-    model.push(recipe);  
+    //push recipe to recipes
+    recipes.push(recipe);  
 
     //*** call function to transfer recipes held in Model to localstorage recipes
 
-    return model;
+    return recipes;
 };
 
 Model.editRecipe = function(recipe){
-    //find recipe in model
-    let matchPosition = model.findIndex( matchingRecipe => matchingRecipe.id === recipe.id  );
+    //find recipe in recipes
+    let matchPosition = recipes.findIndex( matchingRecipe => matchingRecipe.id === recipe.id  );
     if(matchPosition === -1){
         return { error: "Does not exist" };    
     }
 
-    model[matchPosition] = recipe;   
-    return model;
+    recipes[matchPosition] = recipe;   
+    return recipes;
 };
 
 Model.getRecipeById = function(id){
-    let recipe = model.filter( recipe => recipe.id === id);
+    let recipe = recipes.filter( recipe => recipe.id === id);
     if(recipe.length < 1){
         return { error: "Recipe does not exist" };
     }    
@@ -50,19 +49,19 @@ Model.getRecipeById = function(id){
 };
 
 Model.deleteRecipeById = function(id){
-    //find recipe in model
-    let matchPosition = model.findIndex( recipe => recipe.id === id );
+    //find recipe in recipes
+    let matchPosition = recipes.findIndex( recipe => recipe.id === id );
     if(matchPosition === -1){
         return { error: "Recipe does not exist" };
     }
 
     //splice it out
-    model.splice(matchPosition, 1);
-    return model;
+    recipes.splice(matchPosition, 1);
+    return recipes;
 };
 
 function getRecipesFromStorage(){
-    return model;
+    return recipes;
 }
 
 function addRecipesToStorage(){
