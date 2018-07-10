ed307766c6bfeb6200710e567880e955c9f0ea62
Carl Hain <crhain@gmail.com>
2018-07-10 14:33:17 -0500

    Add stub function  getRecipesFromSTorage and addRecipesToStorage functions in model/index.js; these will handle adding and retrieving recipes from localstorage

--- src/model/index.js
+++ src/model/index.js
@@ -4,17 +4,31 @@ import model from "model/seed.js";
 let Model = {};
 
 Model.getAllRecipes = function(){
+    //1. check to see if model has items in it
+    
+    //1a. if not, get recipes key from local storage
+    
+    //1a. convert them to JSON
+    
+    //1a. iterate over keys and add to Model
+
+    //2. return model
     return model;
-}
+};
 
 Model.addRecipe = function(recipe){
-    
+    //look for recipe in model and if it exists, then return error
     if(model.findIndex( item => item.id === recipe.id ) > -1){    
         return { error: "Already exists" };
     }   
+
+    //push recipe to model
     model.push(recipe);  
+
+    //*** call function to transfer recipes held in Model to localstorage recipes
+
     return model;
-}
+};
 
 Model.editRecipe = function(recipe){
     //find recipe in model
@@ -25,7 +39,7 @@ Model.editRecipe = function(recipe){
 
     model[matchPosition] = recipe;   
     return model;
-}
+};
 
 Model.getRecipeById = function(id){
     let recipe = model.filter( recipe => recipe.id === id);
@@ -33,7 +47,7 @@ Model.getRecipeById = function(id){
         return { error: "Recipe does not exist" };
     }    
     return recipe[0];    
-}
+};
 
 Model.deleteRecipeById = function(id){
     //find recipe in model
@@ -45,6 +59,14 @@ Model.deleteRecipeById = function(id){
     //splice it out
     model.splice(matchPosition, 1);
     return model;
+};
+
+function getRecipesFromStorage(){
+    return model;
+}
+
+function addRecipesToStorage(){
+
 }
 
 export default Model;
