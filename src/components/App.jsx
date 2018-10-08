473646368ec7e927ba6d7d3d18f909c36c100ab8
Carl Hain <crhain@gmail.com>
2018-10-08 11:45:37 -0500

    Add materialize directly as npm package; load materialize css directly into app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Model from 'model/'
 import Root from 'components/Root';
 import RecipeList from 'components/RecipeList';
 import RecipeView from 'components/Recipe/RecipeView';
 import RecipeEdit from 'components/Recipe/Edit';
 import RecipeNew from 'components/Recipe/New';
 import 'components/App.css';
+import '../../node_modules/materialize-css/dist/css/materialize.min.css';
+import '../../node_modules/materialize-css/dist/js/materialize.min.js';
 
-class App extends Component {
-
+class App extends Component {  
   render() {
     return (          
         <Root>
