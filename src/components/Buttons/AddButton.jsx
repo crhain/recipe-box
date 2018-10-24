069a4f8c2fdabafb7d8c3f115c40377c76e49247
Carl Hain <crhain@gmail.com>
2018-10-24 16:54:28 -0500

    Change AddButton to class component and add tooltip

--- src/components/Buttons/AddButton.jsx
+++ src/components/Buttons/AddButton.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import M from '../../../node_modules/materialize-css/dist/js/materialize.min.js';
 
-export default function AddButton(props){
-    return (
-        <Link 
-            className="waves-effect waves-light btn add-button"             
-            to="/recipe/new"
-        ><i className="material-icons">add</i>            
-        </Link>
-    );    
+export default class AddButton extends Component{
+    componentDidMount(){        
+        var elems = document.querySelectorAll('.tooltipped');        
+        M.Tooltip.init(elems, {enterDelay: 800});             
+    } 
+    render(){
+        return (
+            <Link 
+                className="waves-effect waves-light btn add-button tooltipped" 
+                data-position="bottom"
+                data-tooltip="New Recipe"            
+                to="/recipe/new"
+            ><i className="material-icons">add</i>            
+            </Link>
+        );    
+    }
+    
 }
