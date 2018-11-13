23073437476d52d225129c0dfbeea330bc728344
Carl Hain <crhain@gmail.com>
2018-11-13 14:16:12 -0600

    Add home button to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import AddButton from "components/Buttons/AddButton";
+import HomeButton from "components/Buttons/HomeButton";
 import DeleteModal from "components/DeleteModal";
 
 class Header extends Component {
     render(){
         return (
             <div>
-                <AddButton />
+                <HomeButton />
+                <AddButton />                
                 <DeleteModal acceptHandler={this.deleteRecipe} >
                     <h4>Delete Confirmation</h4>
                     <p>Do you wish to delete this recipe?</p>
