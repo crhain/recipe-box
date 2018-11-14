9e5ffaa479fa8c4dab3c2a71b9728e6fa88938cb
Carl Hain <crhain@gmail.com>
2018-11-14 14:50:12 -0600

    Update seed.js with new ingredients structure

--- src/model/seed.js
+++ src/model/seed.js
@@ -6,17 +6,17 @@ export default  [
         image: "https://tiphero.com/wp-content/uploads/2017/05/easy-beef-stroganoff-FI-750x364.jpg",
         servings: 4,
         ingredients: [
-            "1 1/2 pounds beef sirloin steak", 
-            "8 ounces fresh mushrooms",
-            "2 medium onions, thinly sliced",
-            "1 garlic clove, finely chopped",
-            "1/2 cup butter",
-            "1 1/2 cups Progresso, beef flavored broth",
-            "1/2 teaspoon salt",
-            "1 teaspoon Worcestshire sauce",
-            "1/4 cup Gold Medal all-purpose flour",
-            "1 1/2 cups sour cream",
-            "3 cups hot cooked egg noodles"
+            {ingredient: "1 1/2 pounds beef sirloin steak"},
+            {ingredient: "8 ounces fresh mushrooms"},
+            {ingredient: "2 medium onions, thinly sliced"},
+            {ingredient: "1 garlic clove, finely chopped"},
+            {ingredient: "1/2 cup butter"},
+            {ingredient: "1 1/2 cups Progresso, beef flavored broth"},
+            {ingredient: "1/2 teaspoon salt"},
+            {ingredient: "1 teaspoon Worcestshire sauce"},
+            {ingredient: "1/4 cup Gold Medal all-purpose flour"},
+            {ingredient: "1 1/2 cups sour cream"},
+            {ingredient: "3 cups hot cooked egg noodles"}            
         ]
     },
     {
@@ -26,23 +26,23 @@ export default  [
         image: "https://www.cscassets.com/recipes/wide_cknew/wide_60207.jpg",
         servings: 4,
         ingredients: [
-            "1 1/4 pounds Johnsonville Ground Mild Italian sausage",
-            "3/4 pound ground beef",
-            "1 medium onion, diced",
-            "3 garlic cloves, minced",
-            "2 cans (one 28 ounces, one 15 ounces) crushed tomatoes",
-            "2 cans (6 ounces each) tomato paste",
-            "2/3 cup water",
-            "2 to 3 tablespoons sugar",
-            "3 tablespoons plus 1/4 cup minced fresh parsley, divided",
-            "2 teasponns dried basil",
-            "3/4 teaspoon fennel seed",
-            "3/4 teaspoon salt, divided",
-            "1/4 teaspoon coarsely ground pepper",
-            "1 large egg, lightly beaten",
-            "1 carton (15 ounces) ricotta cheese",
-            "4 cups shredded part-skim mozzarella cheese",
-            "3/4 cup grated Parmesan cheese"
+            {ingredient: "1 1/4 pounds Johnsonville Ground Mild Italian sausage"},
+            {ingredient: "3/4 pound ground beef"},
+            {ingredient: "1 medium onion, diced"},
+            {ingredient: "3 garlic cloves, minced"},
+            {ingredient: "2 cans (one 28 ounces, one 15 ounces) crushed tomatoes"},
+            {ingredient: "2 cans (6 ounces each) tomato paste"},
+            {ingredient: "2/3 cup water"},
+            {ingredient: "2 to 3 tablespoons sugar"},
+            {ingredient: "3 tablespoons plus 1/4 cup minced fresh parsley, divided"},
+            {ingredient: "2 teasponns dried basil"},
+            {ingredient: "3/4 teaspoon fennel seed"},
+            {ingredient: "3/4 teaspoon salt, divided"},
+            {ingredient: "1/4 teaspoon coarsely ground pepper"},
+            {ingredient: "1 large egg, lightly beaten"},
+            {ingredient: "1 carton (15 ounces) ricotta cheese"},
+            {ingredient: "4 cups shredded part-skim mozzarella cheese"},
+            {ingredient: "3/4 cup grated Parmesan cheese"}                                                            
         ]
     },
     {
@@ -52,14 +52,14 @@ export default  [
         image: "",
         servings: 4,
         ingredients: [
-            "2 tablespoons olive oil",
-            "1 onion, finely chopped (about 1 cup)",
-            "1 garlic clove, minced",
-            "2 cups of medium or long-grain white rice",
-            "3 cups chicken stock",
-            "1 heaping tablespoon tomato paste or 1 cup of diced fresh or cooked tomatoes, strained",
-            "Pinch of oregano",
-            "1 teaspoon salt"
+            {ingredient: "2 tablespoons olive oil"},
+            {ingredient: "1 onion, finely chopped (about 1 cup)"},
+            {ingredient: "1 garlic clove, minced"},
+            {ingredient: "2 cups of medium or long-grain white rice"},
+            {ingredient: "3 cups chicken stock"},
+            {ingredient: "1 heaping tablespoon tomato paste or 1 cup of diced fresh or cooked tomatoes, strained"},
+            {ingredient: "Pinch of oregano"},
+            {ingredient: "1 teaspoon salt"}            
         ]
     }
 ];
