f79aa202aadebf765720b2c84328db7714caa852
Carl Hain <crhain@gmail.com>
2018-11-16 10:19:24 -0600

    Update recipeReducer.test.js to pull test data from seed.js

--- src/reducers/__tests__/recipeReducer.test.js
+++ src/reducers/__tests__/recipeReducer.test.js
@@ -1,24 +1,9 @@
 import recipeReducer from "reducers/recipeReducer";
 import { GET_RECIPE } from "actions/types";
 import Model from "model";
+import seed from "model/seed.js";
 
-const beef = {
-    id: "beefstrogonoff", 
-    title: "Beef Strogonoff", 
-    ingredients: [
-        "1 1/2 pounds beef sirloin steak", 
-        "8 ounces fresh mushrooms",
-        "2 medium onions, thinly sliced",
-        "1 garlic clove, finely chopped",
-        "1/2 cup butter",
-        "1 1/2 cups Progresso, beef flavored broth",
-        "1/2 teaspoon salt",
-        "1 teaspoon Worcestshire sauce",
-        "1/4 cup Gold Medal all-purpose flour",
-        "1 1/2 cups sour cream",
-        "3 cups hot cooked egg noodles"
-    ]
-};
+const beef = seed[0];
 
 
 describe("recipeReducer", () => {
