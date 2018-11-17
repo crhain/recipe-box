231459b6a8fa0519e5b5d7358c05f72c00ccbb95
Carl Hain <crhain@gmail.com>
2018-11-17 10:22:25 -0600

    Fix error on RecipeFormIngredientEntry that was preventing ingredient entries being added when 'add recipe' button was pressed; update styling of buttons

--- src/components/Form/RecipeFormIngredientEntry.jsx
+++ src/components/Form/RecipeFormIngredientEntry.jsx
@@ -3,30 +3,35 @@ import { Field } from 'redux-form';
 import FormTextField from 'components/Form/FormTextField';
 
 const renderField = ( { input, label, type, meta: {touched, error } }) => {
-    <div className="form__text-field">
+    return (<div className="form__text-field">
         <label>{label}</label>
         <input {...input} style={{ marginBottom: '5px' }} /> 
         <div style={{ marginBottom: '20px', color: 'red' }}>  
             {touched && error} 
         </div>    
-    </div>
+    </div>);
 };
 
-export default ({ fields, input, label, meta: { error, touched } }) => {    
+export default ({ fields }) => {    
     return (
         <ul>
             <li>
-                <button type="button" onClick={ () => fields.push({}) }>
+                <button 
+                    className="waves-effect waves-light btn"
+                    type="button" 
+                    onClick={ () => fields.push({}) }
+                >
                     Add Ingredient
                 </button>                
             </li>
             {fields.map( (ingredient, index) => (
                 <li key={index}>
                     <button
-                        type="button"
-                        title="Remove Ingredient"
+                        className="waves-effect waves-light btn delete-button"
                         onClick={ () => fields.remove(index) }
-                     />
+                     >
+                        <i className="material-icons">delete</i>
+                     </button>
                      <Field 
                         name={`${ingredient}.ingredient`}
                         type="text"
