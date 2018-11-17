e815f09bb9693db009e20c73285d5f36fc875212
Carl Hain <crhain@gmail.com>
2018-11-17 10:08:25 -0600

    Update RecipeFormIngredientEntry to cointain components for adding an ingredient; add ingredient button currently does not do anything

--- src/components/Form/RecipeFormIngredientEntry.jsx
+++ src/components/Form/RecipeFormIngredientEntry.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Field } from 'redux-form';
+import FormTextField from 'components/Form/FormTextField';
 
-export default ({ input, label, meta: { error, touched } }) => {    
+const renderField = ( { input, label, type, meta: {touched, error } }) => {
+    <div className="form__text-field">
+        <label>{label}</label>
+        <input {...input} style={{ marginBottom: '5px' }} /> 
+        <div style={{ marginBottom: '20px', color: 'red' }}>  
+            {touched && error} 
+        </div>    
+    </div>
+};
+
+export default ({ fields, input, label, meta: { error, touched } }) => {    
     return (
-        <div className="form__text-field">
-            <label>{label}</label>
-            <input {...input} style={{ marginBottom: '5px' }} /> 
-            <div style={{ marginBottom: '20px', color: 'red' }}>  
-                {touched && error} 
-            </div>    
-        </div>
+        <ul>
+            <li>
+                <button type="button" onClick={ () => fields.push({}) }>
+                    Add Ingredient
+                </button>                
+            </li>
+            {fields.map( (ingredient, index) => (
+                <li key={index}>
+                    <button
+                        type="button"
+                        title="Remove Ingredient"
+                        onClick={ () => fields.remove(index) }
+                     />
+                     <Field 
+                        name={`${ingredient}.ingredient`}
+                        type="text"
+                        component={renderField}
+                        label="Ingredient"
+                     />
+                </li>                
+            ) )}
+        </ul>     
     );
 };
