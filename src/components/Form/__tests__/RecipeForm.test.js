5d8bef9820d27fd043bba378d68840542b97cb03
Carl Hain <crhain@gmail.com>
2018-11-23 10:36:46 -0600

    Add test to RecipeForm.test.js to see that RecipeFormInstructionEntry renders

--- src/components/Form/__tests__/RecipeForm.test.js
+++ src/components/Form/__tests__/RecipeForm.test.js
@@ -5,7 +5,7 @@ import FormTextField from "components/Form/FormTextField";
 import FormTextBoxField from "components/Form/FormTextBoxField";
 import FormInvisibleTextField from "components/Form/FormInvisibleTextField";
 import RecipeFormIngredientEntry from "components/Form/RecipeFormIngredientEntry";
-
+import RecipeFormInstructionEntry from "components/Form/RecipeFormInstructionEntry";
 
 let component; 
 let meta = {error: null, touched: false};
@@ -94,6 +94,25 @@ describe("RecipeFormIngredientEntry", () => {
     beforeEach(() => {
         component = shallow(
             <RecipeFormIngredientEntry 
+                input={input}
+                label={"test"}
+                meta={ meta } 
+                fields={[]}
+            />          
+        );
+    });
+
+    it('renders correctly', () => {
+        expect(component.exists()).toEqual(true);
+    });
+    
+});
+
+describe("RecipeFormInstructionEntry", () => {
+        
+    beforeEach(() => {
+        component = shallow(
+            <RecipeFormInstructionEntry 
                 input={input}
                 label={"test"}
                 meta={ meta } 
