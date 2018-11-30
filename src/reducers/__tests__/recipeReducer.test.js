3a000f6b6d14a21f288f7c5cd6f075e1045474b2
Carl Hain <crhain@gmail.com>
2018-11-30 12:09:11 -0600

    Rewrite recipeReducer.test.js to only test for case where empty action is sent.  All other tests are not true unit tests because they are also testing functions in model/index.js

--- src/reducers/__tests__/recipeReducer.test.js
+++ src/reducers/__tests__/recipeReducer.test.js
@@ -1,28 +1,11 @@
 import recipeReducer from "reducers/recipeReducer";
-import { GET_RECIPE } from "actions/types";
 import Model from "model";
-import seed from "model/seed.js";
-
-const beef = seed[0];
 
 
-describe("recipeReducer", () => {
-    
-    // it("Should return initial state of null when no action type passed and inital state not set", () => {
-    //     expect(recipeReducer(undefined, {type: null, payload: null})).toEqual(null);
-    // });
-    Model.toggleTestMode(true);
+Model.toggleTestMode(true);
 
-    describe("GET_RECIPE", () => {
-                   
-        it("Should return the correct recipe when passed id for recipe in action", () => {        
-            expect( recipeReducer( undefined, { type: GET_RECIPE, id: "beefstrogonoff" } ) ).toMatchObject(beef);
-        });
-
-        // it("should return null if id not found", () => {
-        //     expect( recipeReducer( undefined, { type: GET_RECIPE, id: "beefstrogonoffff" } ) ).toEqual(null);
-        // });
-
-    });
-            
+describe("recipeReducer", () => {    
+    it("Should return initial state of [] when no action type passed and inital state not set", () => {
+        expect(recipeReducer(undefined, {type: null, payload: null}).length).toEqual(0);
+    });             
 });
