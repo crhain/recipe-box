e706e6c8eac999d4b7fe883fab035c924c6110b6
Carl Hain <crhain@gmail.com>
2018-12-02 10:38:21 -0600

    Add tests to recipeListReducer.test.js for cases: ADD_RECIPE, EDIT_RECIPE, and DELETE_RECIPE

--- src/reducers/__tests__/recipeListReducer.test.js
+++ src/reducers/__tests__/recipeListReducer.test.js
@@ -6,9 +6,19 @@ Model.toggleTestMode(true);
 
 const mockModel = {
     getAllRecipes: () => { return {actionType: "GET_RECIPES"} },
-    addRecipe: () => { return {actionType: "ADD_RECIPE"} },
-    editRecipe: () => { return {actionType: "EDIT_RECIPE"} },
-    deleteRecipeById: () => { return {actionType: "DELETE_RECIPE"} }
+    addRecipe: (recipe) => { return {actionType: "ADD_RECIPE"} },
+    editRecipe: (recipe) => { return {actionType: "EDIT_RECIPE"} },
+    deleteRecipeById: (recipeId) => { return {actionType: "DELETE_RECIPE"} }
+}
+
+const mockRecipe = {
+    title: "",
+    description: "",
+    image: "",
+    servings: "",
+    id: "",
+    ingredients: [],
+    instructions: []
 }
 
 const mockHistory = [];
@@ -20,6 +30,18 @@ describe("recipeListReducer", () => {
     });
     
     it("Should call GET_RECIPES case when passed an action with GET_RECIPES type", () => {
-        expect(recipeListReducer(undefined, {type: GET_RECIPES, payload: []}, mockModel).actionType).toEqual("GET_RECIPES");
+        expect(recipeListReducer(undefined, {type: GET_RECIPES, payload: [], history: mockHistory}, mockModel).actionType).toEqual("GET_RECIPES");
+    });
+
+    it("Should call ADD_RECIPE case when passed an action with ADD_RECIPE type", () => {
+        expect(recipeListReducer(undefined, {type: ADD_RECIPE, recipe: mockRecipe, history: mockHistory}, mockModel).actionType).toEqual("ADD_RECIPE");
+    });
+
+    it("Should call EDIT_RECIPE case when passed an action with EDIT_RECIPE type", () => {
+        expect(recipeListReducer(undefined, {type: EDIT_RECIPE, recipe: mockRecipe, history: mockHistory}, mockModel).actionType).toEqual("EDIT_RECIPE");
+    });
+
+    it("Should call DELETE_RECIPE case when passed an action with DELETE_RECIPE type", () => {
+        expect(recipeListReducer(undefined, {type: DELETE_RECIPE, id: mockRecipe.id, history: mockHistory}, mockModel).actionType).toEqual("DELETE_RECIPE");
     });
 });
