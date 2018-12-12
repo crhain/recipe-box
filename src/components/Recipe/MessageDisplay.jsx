9e3c2fc806f6a9ad667e25743f703217deefe878
Carl Hain <crhain@gmail.com>
2018-12-12 11:36:12 -0600

    Add MessageDisplay component for displaying failure and success messages when interacting with model/server

---
+++ src/components/Recipe/MessageDisplay.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+        <div className="message-display">
+            This is the message display
+        </div>
+    );
+};
