e00004319ae9b0acac9eb95f82a6aa4d1bcfacb2
Carl Hain <crhain@gmail.com>
2018-12-13 11:42:38 -0600

    Add messageDisplay item to state and send it results of messageDisplayReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { reducer as formReducer } from 'redux-form';
 import RecipeListReducer from 'reducers/recipeListReducer';
 import RecipeReducer from "reducers/recipeReducer";
 import ModalReducer from "reducers/modalReducer";
+import MessageDisplayReducer from "reducers/messageDisplayReducer";
 
 
 const rootReducer = combineReducers({
   recipes: RecipeListReducer,
   selectedRecipe: RecipeReducer,
   recipeToDelete: ModalReducer,
+  messageDisplay: MessageDisplayReducer,
   form: formReducer
 });
 
