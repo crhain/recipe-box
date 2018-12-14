6cf35399ba7a0b8f21060f584d0008f8319ad1c5
Carl Hain <crhain@gmail.com>
2018-12-14 09:34:59 -0600

    Change MessageDisplay component to connected component and hook it up to messageDisplay state

--- src/components/MessageDisplay.jsx
+++ src/components/MessageDisplay.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default (props) => {
-    return (
-        <div className="message-display">          
-        </div>
-    );
-};
+class MessageDisplay extends Component{
+    render () {
+        return (
+            <div className="message-display"> 
+                {this.props.messageDisplay}         
+            </div>
+        );
+    };
+}
+
+function mapStateToProps({ messageDisplay }){
+    return { messageDisplay };
+}
+
+export default connect(mapStateToProps)(MessageDisplay);
