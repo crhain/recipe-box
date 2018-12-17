8ce7aa768a12a23f403748fa25cd857173979560
Carl Hain <crhain@gmail.com>
2018-12-17 12:58:27 -0600

    Add redux-flash middleware to store

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import { middleware as flashMiddleware } from 'redux-flash';
 import reducers from 'reducers';
 
-export const store = createStore(reducers, {}, applyMiddleware());
+export const store = createStore(reducers, {}, applyMiddleware(flashMiddleware()));
 
 export const Root = function(props){
     return (
