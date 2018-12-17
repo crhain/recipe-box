ee4c14fb8207c4ebb5d4579ba987266a8eaaa9e0
Carl Hain <crhain@gmail.com>
2018-12-17 13:17:38 -0600

    Remove messageReducer and tests for messageReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,6 @@ import RecipeListReducer from 'reducers/recipeListReducer';
 import RecipeReducer from "reducers/recipeReducer";
 import ModalReducer from "reducers/modalReducer";
 import { reducer as flashReducer } from 'redux-flash';
-import MessageDisplayReducer from "reducers/messageDisplayReducer";
-
 
 const rootReducer = combineReducers({
   recipes: RecipeListReducer,
