d9c47c74c2480c8528d8e54e55d459880533d1f2
Carl Hain <crhain@gmail.com>
2018-12-22 18:36:45 -0600

    Remove unused imports from MessageDisplay.jsx

--- src/components/MessageDisplay.jsx
+++ src/components/MessageDisplay.jsx
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { getLatestMessage } from 'redux-flash';
+import React from "react";
+import { connect } from "react-redux";
+import { getLatestMessage } from "redux-flash";
 
 function MessageDisplay({ flash }) {
-    return (
-        <div className="message-display">
-            {
-                flash && <div>{flash.message} </div>
-            }
-        </div>
-    );
+  return (
+    <div className="message-display">
+      {flash && <div>{flash.message} </div>}
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
-    return { flash: getLatestMessage(state) };
+  return { flash: getLatestMessage(state) };
 }
 
 export default connect(mapStateToProps)(MessageDisplay);
