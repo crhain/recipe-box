3a1edb8818a432a2000bb303c0d6e12274f72c8b
Carl Hain <crhain@gmail.com>
2019-01-23 13:25:32 -0600

    Add jsdoc typedefs for Recipe and Ingreident back to model/index.js

--- src/model/index.js
+++ src/model/index.js
@@ -1,4 +1,24 @@
-// @nots-check
+// @ts-check
+
+/**
+ * @typedef Ingredient
+ * @type {Object}
+ * @property {Number} quantity
+ * @property {String} measure
+ * @property {String} ingredient
+ */
+
+/**
+ * @typedef Recipe
+ * @type {Object}
+ * @property {String} id - an ID
+ * @property {String} title - recipe title
+ * @property {String} description - recipe description
+ * @property {String} image - url for image
+ * @property {Number} servings - number of servings
+ * @property {Array.<Ingredient>} ingredients - list of ingredient objects
+ * @property {Array.<String>} instructions - list of instruction objects
+ */
 
 import seed from 'model/seed.js';
 import { RecipeLimitReached, RecipeDoesNotExist } from 'model/error.js';
