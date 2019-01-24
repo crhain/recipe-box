3a560476a6fcfe874a9096484fef59511a29600a
Carl Hain <crhain@gmail.com>
2019-01-24 09:54:12 -0600

    Change Header.jsx to a functional component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,22 +5,20 @@ import HomeButton from 'components/Buttons/HomeButton';
 import DeleteModal from 'components/DeleteModal';
 
 /**
- * @description functional component for Header component
+ * @description functional component for Header element
  * @return {*} jsx element
  */
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <HomeButton />
-        <AddButton />
-        <DeleteModal>
-          <h4>Delete Confirmation</h4>
-          <p>Do you wish to delete this recipe?</p>
-        </DeleteModal>
-      </div>
-    );
-  }
-}
+const Header = function(props) {
+  return (
+    <div>
+      <HomeButton />
+      <AddButton />
+      <DeleteModal>
+        <h4>Delete Confirmation</h4>
+        <p>Do you wish to delete this recipe?</p>
+      </DeleteModal>
+    </div>
+  );
+};
 
 export default Header;
