df2b8177c8f32832cf2ea3bde99cb846fabf3577
Carl Hain <crhain@gmail.com>
2019-01-25 10:13:21 -0600

    Add jsdoc comments to RecipeList.jsx

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,10 +2,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import { getRecipes } from 'actions/';
+import { getRecipes } from 'actions/index';
 import EditButton from 'components/Buttons/EditButton';
 import DeleteButton from 'components/Buttons/DeleteButton';
 
+/**
+ * @description class component that returns a recipe list
+ * @extends Component
+ */
 export class RecipeList extends Component {
   constructor(props) {
     super(props);
@@ -13,10 +17,19 @@ export class RecipeList extends Component {
     this.deleteRecipe = this.props.deleteRecipe;
   }
 
+  /**
+   * @description lifecycle method that fires when component mounts
+   *  fires getRecipe action to populate component
+   */
   componentDidMount() {
     this.props.getRecipes();
   }
 
+  /**
+   * @description helper method that creates recipe items to populate
+   *  recipe list
+   * @return {*} jsx element
+   */
   createRecipeList() {
     if (this.props.recipes.length > 0) {
       return this.props.recipes.map((recipe, index) => {
@@ -38,6 +51,10 @@ export class RecipeList extends Component {
     }
   }
 
+  /**
+   * @description render method
+   * @return {*} jsx element
+   */
   render() {
     return (
       <div className="recipe-list container">
@@ -50,6 +67,11 @@ export class RecipeList extends Component {
   }
 }
 
+/**
+ * @description maps state from redux object onto  props
+ * @param {*} param0 - destructured items from state redux object
+ * @return {Object} prop object
+ */
 const mapStateToProps = function({ recipes }) {
   return { recipes };
 };
