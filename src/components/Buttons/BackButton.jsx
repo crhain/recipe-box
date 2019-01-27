bfde5285e45bac34601461c7ce63b427e21034c0
Carl Hain <crhain@gmail.com>
2019-01-27 09:22:37 -0600

    Add jsdoc comments to BackButton.jsx

--- src/components/Buttons/BackButton.jsx
+++ src/components/Buttons/BackButton.jsx
@@ -1,17 +1,34 @@
-// @nots-check
+// @ts-check
 import React, { Component } from 'react';
 
+/**
+ * @describe class for a Return button that returns to the previous
+ *  page visited
+ * @extends Component
+ */
 export default class ReturnButton extends Component {
+  /**
+   * @constructor
+   * @param {Object} props - contains properties for component
+   */
   constructor(props) {
     super(props);
     this.handleBackClick = this.handleBackClick.bind(this);
     this.history = props.history;
   }
 
+  /**
+   * @describe handles button click
+   */
+
   handleBackClick() {
     this.history.goBack();
   }
 
+  /**
+   * @describe render function for component
+   * @return {*} jsx component
+   */
   render() {
     return (
       <button
