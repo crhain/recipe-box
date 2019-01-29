94b767dd5008fe56d6085a09c6efafa0f96029f0
Carl Hain <crhain@gmail.com>
2019-01-29 09:42:53 -0600

    Add jsx comments to EditButton.jsx

--- src/components/Buttons/EditButton.jsx
+++ src/components/Buttons/EditButton.jsx
@@ -1,18 +1,34 @@
-// @nots-check
+//@ts-check
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import M from '../../../node_modules/materialize-css/dist/js/materialize.min.js';
 
+/**
+ * @description class that returns an Edit Button
+ * @extends Component
+ */
 export default class EditButton extends Component {
+  /**
+   * @constructor
+   * @param {Object} props
+   */
   constructor(props) {
     super(props);
     this.recipeId = props.recipeId;
   }
 
+  /**
+   * @description lifecycle method that fires after component mounts
+   */
   componentDidMount() {
     var elems = document.querySelectorAll('.tooltipped');
     M.Tooltip.init(elems, { enterDelay: 800 });
   }
+
+  /**
+   * @description method that returns rendered component
+   * @return {*} jsx component
+   */
   render() {
     return (
       <Link
